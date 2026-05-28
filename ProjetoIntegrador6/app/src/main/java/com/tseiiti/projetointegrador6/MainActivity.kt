8dab0f0e502eee15bb64a8c1d4f327e3310f4a8f
Oscar <tseiiti3@gmail.com>
2026-05-28 12:03:45 -0300

    android local

--- ProjetoIntegrador6/app/src/main/java/com/tseiiti/projetointegrador6/MainActivity.kt
+++ ProjetoIntegrador6/app/src/main/java/com/tseiiti/projetointegrador6/MainActivity.kt
@@ -15,22 +15,12 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-//        val swipeToRefresh: SwipeRefreshLayout = findViewById(R.id.swipeToRefresh)
         val webView: WebView = findViewById(R.id.webView)
-
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
         webView.settings.loadWithOverviewMode = true
         webView.settings.useWideViewPort = true
-//        webView.webViewClient = object : WebViewClient() {
-//            override fun onPageFinished(view: WebView?, url: String?) {
-//                swipeToRefresh.isRefreshing = false
-//            }
-//        }
-//        webView.viewTreeObserver.addOnScrollChangedListener {
-//            swipeToRefresh.isEnabled = (webView.scrollY == 0)
-//        }
 
         val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
@@ -44,12 +34,8 @@ class MainActivity : AppCompatActivity() {
         }
         onBackPressedDispatcher.addCallback(this, callback)
 
+        webView.loadUrl("http://192.168.137.1/")
 //        webView.loadUrl("http://192.168.15.50/")
-        webView.loadUrl("https://projeto-integrador-6.vercel.app/")
-
-//        swipeToRefresh.setOnRefreshListener {
-//            Toast.makeText(this, "Recarregamento da página", Toast.LENGTH_SHORT).show()
-//            webView.reload()
-//        }
+//        webView.loadUrl("https://projeto-integrador-6.vercel.app/")
     }
 }
